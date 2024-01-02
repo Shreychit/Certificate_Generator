ca306a1c3c8b9d1a1dec5707eb3a58eb6ed90f6a
Shreychit <shreyaschit15@gmail.com>
2024-01-03 00:00:17 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import CSVReader from './CSVReader'
+import Radiob from "./components/Radiob";
+
+function App() {
+  const [csvData, setCSVData] = useState([]);
+
+  const getCSVData = (data) => {
+    console.log(data);
+    setCSVData(data);
+  }
+
+  //csvData array variable holds the json objects extracted from the CSV file
+
+  return (
+    <div className="App">
+      <p class="bg-red-500">Something is here</p>
+       <CSVReader csvData={getCSVData}/>
+      <Radiob/>
+    </div>
+
+
+    
+  );
+}
+
+export default App;
